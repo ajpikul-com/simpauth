9c255519a60aaea400fa922af889547f3454951f
Andrew Pikul <ajpikul@gmail.com>
2023-10-23 20:25:42 +0000

    Improve ToReferrer to accept default path

--- utilities.go
+++ utilities.go
@@ -9,11 +9,14 @@ func (d *RedirectHome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, string(*d), 302)
 }
 
-type ToReferrer struct{}
+type ToReferrer string
 
 func (d *ToReferrer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defaultLogger.Debug("Redirecting")
 	defaultLogger.Debug(r.URL.Path)
 	defaultLogger.Debug(r.Header.Get("Referer"))
-	http.Redirect(w, r, r.Header.Get("Referer"), 302)
+	if r.URL.Path != r.Header.Get("Referer") {
+		*d = ToReferrer(r.Header.Get("Referer"))
+	}
+	http.Redirect(w, r, string(*d), 302)
 }
