5b1b6e1f6c716aa4649881a5d05d36df10ac0dad
Andrew Pikul <ajpikul@gmail.com>
2023-10-23 23:34:56 +0000

    Improve readme yet more 3

--- interfaces.go
+++ interfaces.go
@@ -31,11 +31,14 @@ type ReqByCoord interface {
 	// LogOut will empty out user state object, that's all. It should _not_ write a response body.
 	LogOut(w http.ResponseWriter, r *http.Request)
 	// IsLoginAllowed tells uwho to skip login process if, for example, we're already logged in.
-	// hint: you can also use it to hint that user is trying to login. It should _not_ write a response body.
+	// hint: you can use it to hint that the user tried to login. It should _not_ write a response body.
 	IsLoginAllowed(w http.ResponseWriter, r *http.Request) bool
-	// OtherStateAction is a hook that will be called after every other source of user information has been requested. It should _not_ write a response body.
+	// OtherStateAction is a hook that will be called after every other source of user information
+	// has been requested. It should _not_ write a response body.
 	OtherStateAction(w http.ResponseWriter, r *http.Request)
-	// ChangeState will be called if the user has tried to login (with success or not), or loggedout. It must write a response body. `uwho` provides some obvious utility functions (see README.md or utilities.go) that you can use.
+	// ChangeState will be called if the user has tried to login (with success or not), or loggedout.
+	// It must write a response body. `uwho` provides some obvious utility functions (see README.md
+	// or utilities.go) that you can use.
 	ChangeState(w http.ResponseWriter, r *http.Request)
 	// IsUserAuthorized will be called after session is read, the user did not login or logout, and session is updated. It's your job to check the request and see if user is authorized. If true, user will continue to the wrapped handler, Coordinator.DesiredResource. If false, you must write a response body. Maybe redirect user to a login page?
 	IsUserAuthorized(w http.ResponseWriter, r *http.Request) bool
