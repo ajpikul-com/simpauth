ec41afdd54a5d9c00f2ed89c8cbb41c30f41ee74
Andrew Pikul <ajpikul@gmail.com>
2023-11-02 15:34:40 +0000

    Convert to typescript

---
+++ usersessioncookie/scripts/uwho-usc.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCookieAsAny = exports.getCookie = void 0;
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        const part = parts.pop();
+        if (typeof (part) === "undefined")
+            return "";
+        return part.split(';').shift();
+    }
+    return "";
+}
+exports.getCookie = getCookie;
+function getCookieAsAny(name) {
+    const obj = JSON.parse(JSON.parse(atob(getCookie(name))).StateString);
+    return obj;
+}
+exports.getCookieAsAny = getCookieAsAny;
