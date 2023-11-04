3a97a749eb8c0f0fb2e96fae4b4d8da974d15050
Andrew Pikul <ajpikul@gmail.com>
2023-11-04 00:46:16 +0000

    Fix build and publish

--- usersessioncookie/scripts/dist/index.js
+++ usersessioncookie/scripts/dist/index.js
@@ -1,7 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCookieAsAny = exports.getCookie = void 0;
-function getCookie(name) {
+export function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
@@ -12,10 +9,8 @@ function getCookie(name) {
     }
     return "";
 }
-exports.getCookie = getCookie;
-function getCookieAsAny(name) {
+export function getCookieAsAny(name) {
     const obj = JSON.parse(JSON.parse(atob(getCookie(name))).StateString);
     return obj;
 }
-exports.getCookieAsAny = getCookieAsAny;
 //# sourceMappingURL=index.js.map
