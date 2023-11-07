63b99c4a0368e5e4cc89b2771b4ccde000ba304a
Andrew Pikul <ajpikul@gmail.com>
2023-11-07 16:39:32 +0000

    Remove vestigal mocha build command

--- usersessioncookie/scripts/build.sh
+++ usersessioncookie/scripts/build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ "$1" == "test" ]; then
-  npx eslint test/ && npx tsc -p tsconfig.test.json
-else
-  npx eslint src/ && npx tsc -p tsconfig.prod.json
-fi
+npx eslint src/ && npx tsc -p tsconfig.prod.json
